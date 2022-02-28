3f61af5a1d0df684b3b6064944d05594de214b40
Atsushi1216 <ats1216@gmail.com>
2022-02-28 11:02:10 +0000

    [Fix]コメントアウト追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   class Task < ApplicationRecord
+    #空では保存させないためにpresence追加
     validates :title, presence: true
     validates :body, presence: true
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #rootをtasks_indexへ変更
   root to: 'tasks#index'
+  #resoucesでアクションをルーティングに追加
   resources :tasks
 end
