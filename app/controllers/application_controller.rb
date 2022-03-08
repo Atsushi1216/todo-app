834d901607a0776f66f0c9780849337e7c6b8acb
Atsushi1216 <ats1216@gmail.com>
2022-03-08 08:07:00 +0000

    [Add]コメントアウト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    tasks_path #遷移させたいページのpathを記述
+    tasks_path #ログイン後、遷移させたいページのpathを記載
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) #登録の際に名前を追加する
   end
 end
